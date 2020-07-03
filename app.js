247f4daca516143f04baf02e3ed7ce9840d8f0dc
GuidoCerioni <60760576+GuidoCerioni@users.noreply.github.com>
2020-07-02 23:20:46 -0300

    a

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const sequelize = require("sequelize");
+
 const server = express();
-const dataBase = new sequelize("mysql://root:@127.0.0.1:3306/clase49");
 
+var dataBase = require("./DB/initialization");
 
+/** Middlewares generales */
+server.use(bodyParser.json());
+
+/** Endpoints middlewares & helpers */
+const cachtSqlError = (res, err) => {
+  res.status(500).json({
+    mensaje: "Ocurrió un error en la consulta SQL.",
+    errStack: err,
+  });
+};
+const validarIdCancion = (req, res, next) => {
+  const idCancion = parseInt(req.params.idCancion);
+  if (!isNaN(idCancion)) {
+    req.locals = {
+      ...req.locals,
+      idCancion,
+    };
+    next();
+  } else {
+    res.status(400).json({
+      mensaje: "El id debe ser un numero.",
+    });
+  }
+};
+
+server.get("/users", (req, res) => {
+  dataBase
+    .query(`SELECT * FROM users`)
+
+    .then((response) => res.status(200).json(response))
+
+    .catch((err) => cachtSqlError(res, err));
+});
 
 server.listen(3000, () => {
-  console.log("Working");
+  console.log("Express server working");
 });
